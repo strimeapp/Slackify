80b3dc63ad36c303034d8593c9d76a0104a1bdbd
Romain Biard <romain@digitallift.fr>
2017-01-20 15:29:04 -0300

    Creation of the test method in the Auth class

--- src/Strime/Slackify/Api/Auth.php
+++ src/Strime/Slackify/Api/Auth.php
@@ -58,4 +58,42 @@ class Auth extends AbstractApi
 
         return $this;
     }
+
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param  string $token
+     * @return Auth
+     */
+    public function test($token) {
+
+        // Check if the type of the variables is valid.
+        if (!is_string($token)) {
+            throw new InvalidArgumentException("The type of the token variable is not valid.");
+        }
+
+        // Set the arguments of the request
+        $arguments = array(
+            "token" => $token
+        );
+
+        $this->setUrl("auth.test", $arguments);
+
+        // Send the request
+        try {
+            $client = new \GuzzleHttp\Client();
+            $json_response = $client->request('GET', $this->getUrl(), []);
+            $response = json_decode( $json_response->getBody() );
+        }
+        catch (RequestException $e) {
+            throw new RuntimeException('The request to the API failed: '.$e->getMessage(), $e->getCode(), $e);
+        }
+
+        if($response->{'ok'} === FALSE) {
+            throw new RuntimeException('The request to the API failed: '.$response->{'error'}.".");
+        }
+
+        return $this;
+    }
 }
--- tests/Unit/Api/AuthTest.php
+++ tests/Unit/Api/AuthTest.php
@@ -7,7 +7,7 @@ use Strime\Slackify\Exception\InvalidArgumentException;
 
 class AuthTest extends AbstractApiTestCase
 {
-    public function testWrongParametersReturnException()
+    public function testRevokeWithWrongParametersReturnException()
     {
         $api = $this->getApiAuthMock('api-token');
 
@@ -21,7 +21,7 @@ class AuthTest extends AbstractApiTestCase
         $api->revoke(1, "foo");
     }
     
-    public function testTestRequestShouldReturnApiObject()
+    public function testRevokeRequestShouldReturnApiObject()
     {
         $api = $this->getApiAuthMock('api-token');
 
@@ -31,5 +31,31 @@ class AuthTest extends AbstractApiTestCase
             ->will($this->returnValue($api));
 
         $api->revoke('dummy-token');
+    }
+
+    public function testTestWithWrongParametersReturnException()
+    {
+        $api = $this->getApiAuthMock('api-token');
+
+        $failure = new InvalidArgumentException('Failed to send the message.');
+        $api->expects($this->once())
+            ->method('test')
+            ->with(array())
+            ->will($this->throwException($failure));
+
+        $this->setExpectedException('Strime\Slackify\Exception\InvalidArgumentException');
+        $api->test(array());
+    }
+    
+    public function testTestRequestShouldReturnApiObject()
+    {
+        $api = $this->getApiAuthMock('api-token');
+
+        $api->expects($this->once())
+            ->method('test')
+            ->with('dummy-token')
+            ->will($this->returnValue($api));
+
+        $api->test('dummy-token');
     }
 }
