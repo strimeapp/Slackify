db8510db7b8b9478b59fa0032bf92314900bdaea
Romain Biard <romain@digitallift.fr>
2017-01-20 10:55:27 -0300

    Creation of the class API and the first method test

---
+++ src/Strime/Slackify/Api/AbstractApi.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of Slackify.
+ *
+ * (c) Strime <contact@strime.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strime\Slackify\Api;
+
+abstract class AbstractApi implements ApiInterface
+{
+	const SLACK_API_URL = "tps://slack.com/api/METHOD";
+
+
+    /** @var string */
+    protected $token;
+
+
+    public function __construct($token) {
+        $this->token = $token;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getToken() {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     *
+     * @return ApiInterface
+     */
+    public function setToken($token) {
+
+        if (!is_string($token)) {
+            throw new InvalidArgumentException('The token must be a string.');
+        }
+
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * @param array $arguments
+     *
+     * @return ApiInterface
+     */
+    private function setUrl($arguments) {
+
+        if (!is_array($arguments)) {
+            throw new InvalidArgumentException('The arguments variable must be an array.');
+        }
+
+        // Set a new array
+        $arguments_list = array();
+
+        foreach ($arguments as $key => $value) {
+        	$arguments_list[] = $key . "=" . $value;
+        }
+
+        $this->url = self::SLACK_API_URL . "?" . implode("&", $arguments_list);
+
+        return $this;
+    }
+    
+}
---
+++ src/Strime/Slackify/Webhooks/Api.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of Slackify.
+ *
+ * (c) Strime <contact@strime.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strime\Slackify\Api;
+
+use Strime\Slackify\Exception\RuntimeException;
+use Strime\Slackify\Exception\InvalidArgumentException;
+use GuzzleHttp\Exception\RequestException;
+
+class Api extends AbstractApi
+{
+
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param  string $error
+     * @param  string $foo
+     * @return SlashCommand
+     */
+    public function test($error = NULL, $foo = NULL) {
+
+        // Check if the type of the variables is valid.
+        if (!is_string($error)) {
+            throw new InvalidArgumentException("The type of the error variable is not valid.");
+        }
+        if (!is_string($error)) {
+            throw new InvalidArgumentException("The type of the error variable is not valid.");
+        }
+
+        // Set the arguments of the request
+        $arguments = array();
+
+        if($error != NULL) {
+            $arguments["error"] = $error;
+        }
+        if($foo != NULL) {
+            $arguments["foo"] = $foo;
+        }
+
+        $this->setUrl($arguments);
+
+        // Send the request
+        try {
+            $client = new \GuzzleHttp\Client();
+            $json_response = $client->request('GET', $this->url, [
+                'http_errors' => false
+            ]);
+            $response = json_decode( $json_response->getBody() );
+        }
+        catch (RequestException $e) {
+            throw new RuntimeException('The request to the API failed: '.$e->getMessage(), $e->getCode(), $e);
+        }
+
+        if($response->{'ok'} === FALSE) {
+            throw new RuntimeException('The request to the API failed: '.$response->{'error'}.". Arguments: ".json_encode($response->{'args'}));
+        }
+
+        return $this;
+    }
+}
